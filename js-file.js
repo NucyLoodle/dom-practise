f93746dad0b79f61b19a1d54a4475fb5014753be
Loodle <lucymeredith3@gmail.com>
2022-08-29 07:58:18 +0100

    Change colour of text

--- js-file.js
+++ js-file.js
@@ -4,5 +4,6 @@ const container = document.querySelector('#container');
 const p = document.createElement('div');
 p.classList.add('p');
 p.textContent = "Hey I'm red!";
+p.style.color = 'red';
 
 container.appendChild(p);
