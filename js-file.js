a561e0d836ebed0f409ebb2a259bbb1657f5f684
Loodle <lucymeredith3@gmail.com>
2022-08-29 08:25:06 +0100

    Add div with borders and background

--- js-file.js
+++ js-file.js
@@ -13,3 +13,8 @@ h3.classList.add('h3');
 h3.textContent = "I'm a blue h3!";
 h3.style.color = 'blue';
 container.appendChild(h3);
+
+const newDiv = document.createElement('div');
+newDiv.classList.add('div');
+newDiv.style.cssText = 'border: 3px solid black; background: pink';
+container.appendChild(newDiv);
