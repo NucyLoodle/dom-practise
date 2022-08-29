8a5e08a39c965432c225914d032be26ceeb03b52
Loodle <lucymeredith3@gmail.com>
2022-08-29 07:56:52 +0100

    Add p element to container

--- js-file.js
+++ js-file.js
@@ -1,1 +1,8 @@
 // your JavaScript file
+const container = document.querySelector('#container');
+
+const p = document.createElement('div');
+p.classList.add('p');
+p.textContent = "Hey I'm red!";
+
+container.appendChild(p);
