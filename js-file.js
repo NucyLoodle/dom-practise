dca8ad3420d8879fc0abc43dfbdba323a2fef060
Loodle <lucymeredith3@gmail.com>
2022-08-29 08:49:16 +0100

    Change button blue on click

--- js-file.js
+++ js-file.js
@@ -33,3 +33,6 @@ const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
   alert("Hello World");
 });
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
